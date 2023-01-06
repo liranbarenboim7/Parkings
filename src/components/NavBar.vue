80be76eee0042a5f46af5dba39183f377e63792a
Arcady Barenboim <Arcady.Barenboim@ideo-digital.com>
2023-01-06 18:15:47 +0200

    router

---
+++ src/components/NavBar.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  title: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <button className="navbar-toggler" onClick='{()=>{document.body.style.zoom=0.8;}}' type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" style="border-color:blue"/>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto ">
+        <li class="nav-item">
+         <router-link :to="'/Parkings'" custom v-slot="{ navigate, href }">
+          <a :href="href" @click.stop="navigate">Parkings</a>
+        </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link :to="'/Categories'" custom v-slot="{ navigate, href }">
+            <a :href="href" @click.stop="navigate">Categories</a>
+          </router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+.nav-item {
+  margin-right:10%;
+}
+</style>
---
+++ src/views/parkings.vue
@@ -0,0 +1,188 @@
+<template>
+    <div class="badass-todo">
+    
+      <div class="title has-text-centered">
+        Parkings
+      </div>
+      <form
+      @submit.prevent="isValidFirestoreId(newId) ?  updateParking(newId) : addToParking() "
+      >
+        <div class="field has-addons">
+        <p class="control is-expanded">
+          <input class="input" type="text" placeholder="Add a category" v-model="newCategory">
+          <input class="input" type="text" placeholder="Add a Date" v-model="newDate">
+          <input class="input" type="text" placeholder="From:" v-model="newFrom">
+          <input class="input" type="text" placeholder="To:" v-model="newTo">
+          <input class="input" type="text" placeholder="Price:" v-model="newPrice">
+          <input class="input" type="text" placeholder="Name:" v-model="newName" >
+          <p class="control">
+            <button class="button is-info" :disabled="!newCategory">
+            {{ isValidFirestoreId(newId) ? "update" : "add" }}
+          </button>
+          {{ newId }}
+          </p>
+        </p>
+      </div>
+      </form>
+    
+    
+      <div class="card mb-5" v-for="parking in parkings">
+       <div class="card-content">
+        <div class="content">
+          <div class="columns 
+          is-mobile is-vcentered">
+            <div class="column">
+              {{parking.name}}
+            </div>
+          <div class="column is-5 has-text-right">
+            <button class="button" 
+            @click="toggleDone(parking.id)"
+            >
+              &check;
+            </button>
+            <button 
+            class="button is-danger ml-2"
+            @click="deleteTodo(parking.id)"
+            >
+            &cross;
+          </button>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+    
+    </template>
+    
+    <script setup>
+      import { ref, onMounted } from 'vue'
+      import {db} from '@/firebase'
+      import { collection, onSnapshot,
+        addDoc, doc ,deleteDoc,updateDoc,
+        query, orderBy, limit,setDoc
+      } from "firebase/firestore"
+    
+    const parkings = ref([
+    
+    ])
+    const buttonText= ref('add')
+    const parkingsCollectionRef = collection(db, 'parkings')
+    
+    function isValidFirestoreId(id) {
+      return id.match(/^[a-zA-Z0-9\-_]+$/)
+    }
+    
+    onMounted(() => {
+      onSnapshot(parkingsCollectionRef, (querySnapshot) => {
+        const fbTodos = []
+        querySnapshot.forEach((doc) => {
+          const todo = {
+            id: doc.id,
+            category: doc.data().category,
+            date: doc.data().date,
+            from: doc.data().from,
+            to :doc.data().to,
+            price: doc.data().price,
+            name: doc.data().name
+          }
+          fbTodos.push(todo)
+        })
+        parkings.value = fbTodos
+      })
+    })
+    
+    
+    let newCategory = ref('')
+    const newDate = ref('')
+    const newFrom = ref('')
+    const newTo = ref('')
+    const newPrice = ref('')
+    const newName = ref('')
+    const newId = ref('')
+    
+    const addToParking = () => {
+      addDoc(parkingsCollectionRef, {
+      category: newCategory.value,
+      date: newDate.value,
+      from: newFrom.value,
+      to: newTo.value,
+      price: newPrice.value,
+      name: newName.value
+    
+    });
+      newCategory.value = ''
+      newDate.value = ''
+      newFrom.value = ''
+      newTo.value = ''
+      newPrice.value = ''
+      newName.value = ''
+      newId.value = ''
+    }
+    
+    
+    
+    function updateParking(id) {
+      const frankDocRef = doc(db, "parkings", id);
+      setDoc(frankDocRef, {
+      category: newCategory.value,
+      date: newDate.value,
+      from: newFrom.value,
+      to: newTo.value,
+      price: newPrice.value,
+      name: newName.value
+    });
+    
+    }
+    
+    
+    
+    const deleteTodo = id => {
+      deleteDoc(doc(parkingsCollectionRef, id))
+    }
+    
+    const toggleDone = id => {
+      const index = parkings.value.findIndex(park => park.id === id)
+      newCategory.value = parkings.value[index].category
+      newId.value = id
+      newFrom.value = parkings.value[index].from
+      newTo.value = parkings.value[index].to
+      newPrice.value = parkings.value[index].price
+      newName.value = parkings.value[index].name
+      
+      // updateDoc(doc(parkingsCollectionRef, id), {
+      //    done: !parkings.value[index].done
+      //  });
+    }
+    
+    const getParking = (id) => {
+      db.collection('parkings').doc(id).get().then(function(doc) {
+        if (doc.exists) {
+          console.log("Document data:", doc.data());
+        } else {
+          console.log("No such document!");
+        }
+      }).catch(function(error) {
+        console.log("Error getting document:", error);
+      })
+    }
+    
+    
+    </script>
+    
+    <style>
+    @import 'bulma\css\bulma.min.css';
+    .badass-todo {
+        height: 100%;
+        border: 1px solid;
+        padding: 20px;
+        margin-top: 5em;
+
+        box-shadow: 5px 5px 5px 5px #888888;
+    
+    }
+    
+    .line-through {
+      text-decoration: line-through;
+    }
+    </style>
