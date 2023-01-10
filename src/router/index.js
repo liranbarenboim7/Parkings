b3d33f80d9fe45821d3a0ac79cc4db43dbd5fa37
Arcady Barenboim <Arcady.Barenboim@ideo-digital.com>
2023-01-10 11:34:33 +0200

    id

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Categories  from '../views/categories.vue';
 import Parkings  from '../views/parkings.vue';
+import Parkingdetailes  from '../views/parkingdetailes.vue';
 
 const routes = [
     
     { path: '/', name: 'Home', component: Parkings },
     { path: '/Parkings', name: 'Parkings', component: Parkings },
-    { path: '/Categories', name: 'Categories', component: Categories }
+    { path: '/Categories', name: 'Categories', component: Categories },
+    { path: '/Parking', name: 'Parking', component: Parkingdetailes }
+    
 ];
 
 const router = createRouter({
