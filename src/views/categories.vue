babcfc12101b9055ce0550c03d6fb77cc69a839d
liranbarenboim7 <liranbarenboim7@gmail.com>
2023-01-24 14:16:33 +0200

    24_01_2023

--- src/views/categories.vue
+++ src/views/categories.vue
@@ -5,16 +5,25 @@
         Categories
       </div>
       <form
-      @submit.prevent="isValidFirestoreId(newId) ?  updateParking(newId) : addToParking() "
+      @submit.prevent="isValidFirestoreId(newId) ?  updateCategory(newId) : addToCategory() "
       >
         <div class="field has-addons">
         <p class="control is-expanded">
           <input class="input" type="text" placeholder="Category:" v-model="newCategory">
-          <input class="input" type="text" placeholder="Date:" v-model="newDate">
+         
           <input class="input" type="text" placeholder="Time start:" v-model="newFrom">
           <input class="input" type="text" placeholder="Time End:" v-model="newTo">
-          <input class="input" type="text" placeholder="Action:" v-model="newPrice">
-          <input class="input" type="text" placeholder="Price:" v-model="newName" >
+          <input class="input" type="text" placeholder="Action:" v-model="action">
+          <input class="input" type="text" placeholder="Price:" v-model="newPrice" >
+          
+          <label>Select day:</label>
+          <!--Combobox for days of the week-->
+          <div>
+            <select v-model="newDay">
+              <option v-for="day in days" :value="day">{{ day }}</option>
+            </select>
+          </div>
+
           <p class="control">
             <button class="button is-info" :disabled="!newCategory">
             {{ isValidFirestoreId(newId) ? "update" : "add" }}
@@ -26,23 +35,23 @@
       </form>
     
     
-      <div class="card mb-5" v-for="parking in parkings">
+      <div class="card mb-5" v-for="category in categories">
        <div class="card-content">
         <div class="content">
           <div class="columns 
           is-mobile is-vcentered">
             <div class="column">
-              {{parking.name}}
+              {{category.category}}
             </div>
           <div class="column is-5 has-text-right">
             <button class="button" 
-            @click="toggleDone(parking.id)"
+            @click="showCategory(category.id)"
             >
               &check;
             </button>
             <button 
             class="button is-danger ml-2"
-            @click="deleteTodo(parking.id)"
+            @click="deleteCategory(category.id)"
             >
             &cross;
           </button>
@@ -60,111 +69,111 @@
       import {db} from '@/firebase'
       import { collection, onSnapshot,
         addDoc, doc ,deleteDoc,updateDoc,
-        query, orderBy, limit,setDoc
+        query, orderBy, limit,setDoc,Firestore
       } from "firebase/firestore"
     
-    const parkings = ref([
+    const categories = ref([
     
     ])
+
     const buttonText= ref('add')
-    const parkingsCollectionRef = collection(db, 'categories')
+    const categoriesCollectionRef = collection(db, 'categories')
     
     function isValidFirestoreId(id) {
       return id.match(/^[a-zA-Z0-9\-_]+$/)
     }
     
-    onMounted(() => {
-      onSnapshot(parkingsCollectionRef, (querySnapshot) => {
+    function renderCategory(){
+      onSnapshot(categoriesCollectionRef, (querySnapshot) => {
         const fbTodos = []
         querySnapshot.forEach((doc) => {
           const todo = {
             id: doc.id,
             category: doc.data().category,
-            date: doc.data().date,
+            price: doc.data().price,
+            day: doc.data().day,
             from: doc.data().from,
             to :doc.data().to,
             action: doc.data().action,
           }
           fbTodos.push(todo)
         })
-        parkings.value = fbTodos
+        categories.value = fbTodos
       })
+    }
+
+    onMounted(() => {
+      renderCategory()
     })
     
     
     let newCategory = ref('')
-    const newDate = ref('')
+    const newDay = ref('')
     const newFrom = ref('')
     const newTo = ref('')
     const newPrice = ref('')
     const action = ref('')
     const newId = ref('')
     
-    const addToParking = () => {
-      addDoc(parkingsCollectionRef, {
-      category: newCategory.value,
-      date: newDate.value,
+    //const daySelect = ref('')
+    const days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+
+    const  addToCategory = () => {
+      addDoc(categoriesCollectionRef, {
+      category: newCategory.value,     
+      day: newDay.value,
       from: newFrom.value,
       to: newTo.value,
       price: newPrice.value,
       action: action.value
     });
       newCategory.value = ''
-      newDate.value = ''
+      newDay.value = ''
       newFrom.value = ''
       newTo.value = ''
       newPrice.value = ''
       action.value = ''
       newId.value = ''
+      
     }
     
     
     
-    function updateParking(id) {
-      const frankDocRef = doc(db, "parkings", id);
+    function updateCategory(id) {
+      const frankDocRef = doc(db, "categories", id);
       setDoc(frankDocRef, {
       category: newCategory.value,
-      date: newDate.value,
+      
       from: newFrom.value,
       to: newTo.value,
       price: newPrice.value,
-      action: action.value
+      action: action.value,
+      day: newDay.value
     });
     
     }
     
     
     
-    const deleteTodo = id => {
-      deleteDoc(doc(parkingsCollectionRef, id))
+    const deleteCategory = id => {
+      deleteDoc(doc(categoriesCollectionRef, id))
     }
     
-    const toggleDone = id => {
-      const index = parkings.value.findIndex(park => park.id === id)
-      newCategory.value = parkings.value[index].category
+    const showCategory= id => {
+      const index = categories.value.findIndex(park => park.id === id)
+      newCategory.value = categories.value[index].category
       newId.value = id
-      newFrom.value = parkings.value[index].from
-      newTo.value = parkings.value[index].to
-      newPrice.value = parkings.value[index].price
-      action.value = parkings.value[index].action
-      
+      newFrom.value = categories.value[index].from
+      newTo.value = categories.value[index].to
+      newPrice.value = categories.value[index].price
+      action.value = categories.value[index].action
+      newDay.value = categories.value[index].day
       // updateDoc(doc(parkingsCollectionRef, id), {
       //    done: !parkings.value[index].done
       //  });
     }
     
-    const getParking = (id) => {
-      db.collection('parkings').doc(id).get().then(function(doc) {
-        if (doc.exists) {
-          console.log("Document data:", doc.data());
-        } else {
-          console.log("No such document!");
-        }
-      }).catch(function(error) {
-        console.log("Error getting document:", error);
-      })
-    }
-    
+   
     
     </script>
     
