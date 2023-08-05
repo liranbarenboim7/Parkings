f9a7acb601fd2c65aa05389e79a2a67423771fb1
Arcady Barenboim <Arcady.Barenboim@ideo-digital.com>
2023-08-05 15:36:16 +0300

    street

--- src/views/home.vue
+++ src/views/home.vue
@@ -32,70 +32,28 @@
       <div class="col-lg-6 col-sm-6 col-xs-6" style="height:70vh;overflow-y:auto">
 			<div class="row" v-for="parking in parkings" v-bind:key = parking.id>
 				<div class="col-lg-12 col-sm-12 col-xs-12" data-aos="fade-up">
-					<div class="service">
-						<div class="icon"><i class="fa fa-home" @click="showParking()"></i></div>
-						<h4>Location</h4>
+					<div class="service"  v-on:click="selectParking(parking.id)">
+						<div class="icon"><i class="fa fa-home" @click="selectParking(parking.id)"></i></div>
+						<h4>Parking</h4>
 						<p>         {{ parking.address }}</p>
 					</div>
 				</div><!-- END COL -->
 
 			</div>
       </div>
-      <div class="col-lg-6 col-sm-6 col-xs-6" data-aos="fade-up">
+      <div class="col-lg-6 col-sm-6 col-xs-6" style="height:75vh;overflow-y:auto">
 					<!-- <div class="map" style="width:100%;height:100%" id="map"> -->
-            <div ref="mapDiv" style="width:100%;height:100%" >
+            <div ref="mapDiv" style="width:100%;height: 40vh" >
+             
             </div>
-					
+            <div id="streetDev" ref="streetDiv" style="width: 100%; height: 35vh" />
 				</div><!-- END ROW -->
       </div><!-- END MAIN ROW -->
 
 		</div><!-- END CONTAINER -->
 	</section>
 
-  <!-- <div class="container" style="position:relative;left:8vw;top:22vh;width:180% ; max-height: 90vh">
-
-    <div class="row">
-      <div class="col md-6">
-        <div  v-for="parking in parkings">
-     
-   
   
-              <div class="column">
-                {{ parking.address }}
-              </div>
-
-
-
-        </div>
-
-
-
-        <div class="text-left" style="position:relative;top:200px;height: 20vh">
-          <div class="m-auto" v-if="currPos ">
-            <h4>Your Position</h4>
-            Latitude: {{ currPos.latitude.toFixed(2) }}, Longitude:
-            {{ currPos.longitude.toFixed(2) }}
-          </div>
-          <div class="m-auto">
-            <h4>Distance</h4>
-            {{ distance.toFixed(2) }} miles
-          </div>
-          <div class="m-auto">
-            <h4>Clicked Position</h4>
-            <span v-if="otherPos">
-              Latitude: {{ otherPos.lat.toFixed(4) }}, Longitude:
-              {{ otherPos.lng.toFixed(4) }}
-            </span>
-            <span v-else>Click the map to select a position</span>
-          </div>
-        </div>
-      </div>
-      <div class="col md-6">
-        <div ref="mapDiv" style="position relative;width: 100%; height: 70vh" />
-      </div>
-    </div>
-
-  </div> -->
 
 </template>
 <script setup>
@@ -116,15 +74,19 @@ const parkings = computed(() => store.state.parkingModule.parkingsData);
 
 const otherPos = ref(null)
 //const loader = new Loader({ apiKey: 'AIzaSyDxIpixajq0g7z7NGtftVelLoSeTLtWQc0' })
-const mapDiv = ref(null)
+
 const marker = ref(null)
 let markers = ref([]);
 const formParkingId = computed(() => store.state.parkingModule.selectedParking.id);
 const formParking = computed(() => store.state.parkingModule.selectedParking);
 let myLatlng = { lat: 31.85, lng: 34.76 };
-let map = ref(null)
-let clickListener = null
 
+let clickListener = null
+const mapDiv = ref(null)
+const panorama = ref(null)
+const streetDiv = ref(null)
+let map = ref(null)
+let mapZoom = ref(15)
 onMounted(async () => {
   await store.dispatch("parkingModule/getParkings", {});
   await loader.load()
@@ -138,29 +100,19 @@ onMounted(async () => {
     ({ latLng: { lat, lng } }) =>
       (otherPos.value = { lat: lat(), lng: lng() })
   )
-  if (isSupported)
-  {
-      var watcher = await navigator.geolocation.watchPosition(
-        async (position) => {
-          var coords = position.coords
-          await store.dispatch("gMapModule/UpdateLocation", {
-            currLocation: coords,
-          });
-        })
-  }
+  setTimeout(() => {
+        const fenway = { lat: 32.09, lng: 34.81 };
+        updateStreet(fenway)
+}, 1000);
 })
 onUnmounted(async () => {
   if (clickListener) clickListener.remove()
 })
-async function showParking() {
-
-  // if (map.value && otherPos.value != null) {
-  //   deleteMarkers()
-  //   addMarker(otherPos.value, map.value)
-  //   // The marker, positioned at Uluru
-
-  // }
-}
+const selectParking = async (id) => {
+  await store.dispatch("parkingModule/SelectParking", {
+    selectedParkingId: id,
+  });
+};
 watch(() => formParkingId.value, async () => {
   deleteMarkers()
   const pos = {lat:parseFloat(formParking.value.latitude),lng:parseFloat(formParking.value.longitude)}
@@ -258,4 +210,9 @@ const distance = computed(() =>
 
 //https://developers.google.com/maps/documentation/javascript/examples/places-autocomplete
 </script>
+<style scoped>
+.icon {
+  pointer-events: all;
+}
+</style>
 
