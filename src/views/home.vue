eba84c1a021f8d187aa9aaa828b0a365845269d5
Arcady Barenboim <Arcady.Barenboim@ideo-digital.com>
2023-08-06 11:28:50 +0300

    mobile

--- src/views/home.vue
+++ src/views/home.vue
@@ -26,35 +26,34 @@
       </div>
     </div>
   </header>
-	<section id="service" class="our_services">
-		<div class="container">
-      <div class="row" >
-      <div class="col-lg-6 col-sm-6 col-xs-6" style="height:70vh;overflow-y:auto">
-			<div class="row" v-for="parking in parkings" v-bind:key = parking.id>
-				<div class="col-lg-12 col-sm-12 col-xs-12" data-aos="fade-up">
-					<div class="service"  v-on:click="selectParking(parking.id)">
-						<div class="icon"><i class="fa fa-home" @click="selectParking(parking.id)"></i></div>
-						<h4>Parking</h4>
-						<p>         {{ parking.address }}</p>
-					</div>
-				</div><!-- END COL -->
+  <section id="service" class="our_services">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 col-sm-12 col-xs-12" style="height:75vh;overflow-y:hidden">
+          <!-- <div class="map" style="width:100%;height:100%" id="map"> -->
+          <div ref="mapDiv" style="width:100%;height: 40vh">
 
-			</div>
+          </div>
+          <div id="streetDev" ref="streetDiv" style="width: 100%; height: 35vh" />
+        </div><!-- END col -->
       </div>
-      <div class="col-lg-6 col-sm-6 col-xs-6" style="height:75vh;overflow-y:auto">
-					<!-- <div class="map" style="width:100%;height:100%" id="map"> -->
-            <div ref="mapDiv" style="width:100%;height: 40vh" >
-             
-            </div>
-            <div id="streetDev" ref="streetDiv" style="width: 100%; height: 35vh" />
-				</div><!-- END ROW -->
+      <div class="row">
+        <div class="col-lg-12 col-sm-12 col-xs-12" style="height:70vh;overflow-y:auto">
+          <div class="row" v-for="parking in parkings" v-bind:key=parking.id>
+            <div class="col-lg-12 col-sm-12 col-xs-12 " >
+              <div :class="{active: parking.id == formParkingId }" class="service border" v-on:click="selectParking(parking.id)">
+                <div class="icon"><i class="fa fa-home" @click="selectParking(parking.id)"></i></div>
+                <h4>Parking</h4>
+                <p> {{ parking.address }}</p>
+              </div>
+            </div><!-- END COL -->
+
+          </div>
+        </div>
       </div><!-- END MAIN ROW -->
 
-		</div><!-- END CONTAINER -->
-	</section>
-
-  
-
+    </div><!-- END CONTAINER -->
+  </section>
 </template>
 <script setup>
 import { computed, ref, onMounted, onUnmounted, watch, toRaw } from 'vue'
@@ -92,7 +91,7 @@ onMounted(async () => {
   await loader.load()
   map.value = new google.maps.Map(mapDiv.value, {
     center: myLatlng,
-    zoom: 12
+    zoom: 14
   })
 
   clickListener = map.value.addListener(
@@ -101,9 +100,9 @@ onMounted(async () => {
       (otherPos.value = { lat: lat(), lng: lng() })
   )
   setTimeout(() => {
-        const fenway = { lat: 32.09, lng: 34.81 };
-        updateStreet(fenway)
-}, 1000);
+    const fenway = { lat: 32.09, lng: 34.81 };
+    updateStreet(fenway)
+  }, 1000);
 })
 onUnmounted(async () => {
   if (clickListener) clickListener.remove()
@@ -115,18 +114,18 @@ const selectParking = async (id) => {
 };
 watch(() => formParkingId.value, async () => {
   deleteMarkers()
-  const pos = {lat:parseFloat(formParking.value.latitude),lng:parseFloat(formParking.value.longitude)}
+  const pos = { lat: parseFloat(formParking.value.latitude), lng: parseFloat(formParking.value.longitude) }
   addMarker(pos, map.value)
- 
-    // map.value.setZoom(12);
-    map.value.setCenter(pos);
-    updateStreet(pos)
+
+  // map.value.setZoom(12);
+  map.value.setCenter(pos);
+  updateStreet(pos)
 });
 let line = null
 function updateStreet(pos) {
   const fenway = { lat: 32.09, lng: 34.81 };
   panorama.value = new google.maps.StreetViewPanorama(
-        document.getElementById("streetDev") ,
+    document.getElementById("streetDev"),
     {
       position: pos,
       pov: {
@@ -139,13 +138,13 @@ function updateStreet(pos) {
   map.value.setStreetView(panorama.value);
 
 }
- 
+
 
 watch([parkings], async () => {
   //await loader.load()
   if (parkings && parkings.value) {
     parkings.value.forEach(prk => {
-      addMarker({lat:parseFloat(prk.latitude),lng:parseFloat(prk.longitude)}, map.value)
+      addMarker({ lat: parseFloat(prk.latitude), lng: parseFloat(prk.longitude) }, map.value)
     });
     myLatlng.lat = currPos.value.latitude
     myLatlng.lng = currPos.value.longitude
@@ -213,6 +212,13 @@ const distance = computed(() =>
 <style scoped>
 .icon {
   pointer-events: all;
+  cursor: -webkit-grab;
+  cursor: grab;
+}
+.active
+{
+  background-color:yellow;
+  color:blue;
 }
 </style>
 
